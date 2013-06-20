10ec3efdaf055c18e786c548e7135518a3c457f9
Eathan <Eathan@EATHAN-PC>
2013-06-19 22:17:24 -0400

    first commit.

---
+++ word_trees/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Sentence(models.Model):
+    user_input = models.CharField(max_length=300)
+    
+    def __unicode__(self):
+        return self.user_input
+    
+    def get_words(self):
+        words = self.user_input.split()
+        last_w = Word.objects.get(pk=1)
+        
+        for word in words:
+            child_query = Word.objects.filter(parent = last_w, user_input = word)
+            if child_query:
+                last_w = child_query.get()
+            else:
+                w = Word(user_input=word, sentence=self, parent = last_w)
+                w.save()
+                last_w = w
+            
+    
+        
+        
+    
+class Word(models.Model):
+    sentence = models.ForeignKey(Sentence)
+    user_input = models.CharField(max_length=50)
+#    parent_id = models.IntegerField(default=0)
+    parent = models.ForeignKey('self', default = 1)
+    
+
+    def __unicode__(self):
+        return self.user_input  
+
+    
