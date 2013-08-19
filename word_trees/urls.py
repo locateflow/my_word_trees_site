9cbf3954356f39698f627d9af7ff8c3337342112
Eathan <Eathan@EATHAN-PC>
2013-08-18 19:33:52 -0500

    I finally got the javascript/processing code taking context variables from django!!!!

--- word_trees/urls.py
+++ word_trees/urls.py
@@ -1,10 +1,12 @@
 # Create your views here.
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from word_trees import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<sentence_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<word_id>\d+)/$', views.detail, name='detail'),
 )
 
+urlpatterns += staticfiles_urlpatterns()
